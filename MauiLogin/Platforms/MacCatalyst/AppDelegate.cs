2858ac72731c32d646882598f1944de7196e03ca
AJ Tech <AJ Tech@DESKTOP-RQU8HDL>
2025-01-03 07:56:15 -0800

    Add project files.

---
+++ MauiLogin/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiLogin
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
